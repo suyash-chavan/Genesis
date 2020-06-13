5b408fab153d47b5cb4e1e4f24f220a2c2ba6a69
Suyash Chavan <57249574+suyashc222@users.noreply.github.com>
2020-06-13 12:28:12 +0530

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,104 @@
+@echo off 
+
+mode 1000
+
+title "GENESIS 2020 - Student Management System"
+
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@&                                    @@@@@@@@@@@@@@@@       &@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@  &@@@@@@@@@@@@@@@@@& &@@@@@@@@@@@@@@  @@@@@@@@@@@@@@         @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@  @@@@@@@@@&  @@@@@& &@& &@@@@@@@@@@@  @@@@@@@@@@@@@&           @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@  @@@@@@@&  @@@@@@  @@@@@@  &@@@@@@@@  @@@@@@@@@@@@@            &@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@  @@@@@@   @@@@@&  @@@@@@@&  @@@@@@@@  @@@@@@@@@@@@              @@@@@@@@@@@@@@@@@@@@@               @@@@@@@@@@@@@@@@@              @@@@@@@@@@@                          @@@@@@@@@&             @@@@@@@"
+echo "@@  @@@@@@@@&  &@&  @@@@@@  &@@@@@@@@@@  @@@@@@@@@@@       &@       @@@@@@@@@@@@@@@@@                      @@@@@@@@@@                       @@@@@&                         @@@@@@&                      @"
+echo "@@  @@@@@@@@@@@@@&&@@@@@@@@@@@@@@@@@@@@  @@@@@@@@@@&      @@@@       @@@@@@@@@@@@@&        &@@@@@@@@@        &@@@@@&      &@@@@@@@@&       @@@@@@&      @@@@@@@@@@@@@@@@@  @@@@@      &@@@@@@@@@       &@"
+echo "@@  &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&  @@@@@@@@@&      @@@@@@       @@@@@@@@@@@       &@@@@@@@@@@@@@@@@   &@@@@@@      @@@@@@@@@@@@@@& &@@@@@@@&      @@@@@@@@@@@@@@@@@@@@@@@      @@@@@@@@@@@@@@@  @@@"
+echo "@@  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  @@@@@@@@       &@@@@@@&          &&@@@@&      @@@@@@@@@@@@@@@@@@@&@@@@@@@@      &@@@@@@@@@@@@@@@@@@@@@@@&      @@@@@@@@@@@@@@@@@@@@@@@       @@@@@@@@@@@@@@@@@@@"
+echo "@@& &@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@& &@@@@@@@&       @@@&          &&@@@@@@@@      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@&                  &@@@@@@@@@@&                   @@@@@@@@@@@                   @@@@@@"
+echo "@@@@@@@@@@@@@@  @@@@@@@@@@@   @@@@@@@@@@@@@@@@@&       &           &@&& &@@@@@@@      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                    &@@@@@@@&                   @@@@@@@@@@@@&                    &@@"
+echo "@@@@@@@@@@@@&  @@@&       @@&  @@@@@@@@@@@@@@@@                  @@      @@@@@@@      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                 &@@@@@&      @@@@@@@@@@@  @@@@@@@@@@@@@@@@@                  @"
+echo "@@@@@@@@@                           @@@@@@@@@&                 @@@@       @@@@@@&      @@@@@@@@@@@@@@@@@@@ @@@@@@@@@&&@@@@@@@@@@@@@@@&      @@@@@&      @@@@@@@@@@@@@@@@@@@@@@@@@ @@@@@@@@@@@@@@@@      @"
+echo "@@@@@@@@@  @@@@@@@@@@@@@@@@@@@@@@   @@@@@@@@&                &@@@@@@       @@@@@@       &@@@@@@@@@@@@@@@&   &@@@@@@&  &@@@@@@@@@@@@@@@      @@@@@&      @@@@@@@@@@@@@@@@@@@@@@@@   @@@@@@@@@@@@@@@&     @"
+echo "@@@@@@&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                &@@@@@@@@       @@@@@@&         &&@@@@@&&         @@@@@       &&&@@@@&&&       @@@@@@&      &&&&&&&&&&&&&&&&&& @@@@        &&&@@@@&&       &@"
+echo "@@&        &@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                @@@@@@@@@@@       @@@@@@@@                      @@@@@@@                       &@@@@@@@&                         @@@&                       @@@"
+echo "@@          @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                @@@@@@@@@@@@@       @@@@@@@@@@&&           &@@@@@@@@@@@@@@@@@@&&&         &&&@@@@@@@@@@&&&&&&&&&&&&&&&&&&&&&&&&& @@@@@@@@@@&&          &&@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo(
+echo "You are requested to read Readme.txt first before installing. If you haven't read it quit this window and read that first."
+echo(
+echo(
+echo "          +------------------+---------------+-------------------------------+--------------------------------------------------------------+          "
+echo "          |    Developers    |    Mentors    |         Requirements          |                     Terms and Conditions                     |          "
+echo "          +==================+===============+===============================+==============================================================+          "
+echo "          | Niraja Kulkarni  | Apurva Rawal  | Windows XP or higher          | This project is only for GENESIS.                            |          "
+echo "          | Sanmati Sattur   | Hritik Belani | User should be Administrator  | There should be no Commercial Distribution of this Software. |          "
+echo "          | Suyash Chavan    |               |                               |                                                              |          "
+echo "          +------------------+---------------+-------------------------------+--------------------------------------------------------------+          "
+		
+echo(
+echo "The Software will be automatically installed and its files will be under C:\Student_Management_System\ and there is no provision to change it."
+echo(
+
+set /P sure="Are you Sure to Install this Software (Y or N) ?"
+
+IF NOT "%sure%" == "Y" (
+	echo(
+	echo "This Wizard will automatically Quit in 5 seconds."
+
+	ping 127.0.0.1 -n 6 > nul
+	GOTO END
+)
+
+if exist C:\Student_Management_System\ (
+	echo(
+	echo "This software is already installed in your System. If you need to still Install it, first you need to first Uninstall it."
+	
+	echo(
+
+	echo "This Wizard will automatically Quit in 5 seconds."
+
+	ping 127.0.0.1 -n 6 > nul
+	GOTO END
+) ELSE (
+	echo(
+)
+
+set /p name="Enter username: "
+
+echo(
+echo "Enter Password (Remember the Password): You will not be able to see the Password while you are entering."
+set "psCommand=powershell -Command "$pword = read-host 'Your Password' -AsSecureString ; ^
+    $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword); ^
+        [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)""
+for /f "usebackq delims=" %%p in (`%psCommand%`) do set password=%%p
+echo(
+
+echo "Check your Configuration:"
+
+echo(
+echo Username: %name% 
+
+echo Password: %password%
+
+mkdir C:\Student_Management_System\Database\Students
+mkdir C:\Student_Management_System\Database\Exams
+mkdir C:\Student_Management_System\Database\Attendance
+mkdir C:\Student_Management_System\Database\Schedule
+
+echo @echo off>>C:\Student_Management_System\Database\General_Data.bat
+echo del "C:\Student_Management_System\Database\General_Data.bat">>C:\Student_Management_System\Database\General_Data.bat
+echo %name%>>C:\Student_Management_System\Database\General_Data.bat
+echo %password%>>C:\Student_Management_System\Database\General_Data.bat
+
+
+echo(
+
+echo Software Successfully Installed...
+
+echo(
+
+echo "This Wizard will automatically Quit in 5 seconds."
+
+ping 127.0.0.1 -n 6 > nul
+GOTO END
